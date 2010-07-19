0265ea949d8324a22ddc8da1f2aa1763663530a5
Klaus Kämpf <kkaempf@suse.de>
2010-07-19 20:50:06 +0200

    dot: limit graph to local repo

--- dot.rb
+++ dot.rb
@@ -2,14 +2,19 @@
 #
 def to_dot obs, packages, cycles
   puts "digraph \"#{obs.project}\" {"
+  nhash = {}
   packages.each do |node|
-    name = node['name']
+    nhash[node['name']] = node
+  end
+  nhash.each do |name, node|
 #    puts "# #{node.class}"
     node.xpath("./pkgdep").each do |child|
-      puts "\"#{name}\" -> \"#{child.text.split(' ').first}\";"
+      cname = child.text.split(' ').first
+      next unless nhash[cname]
+      puts "\"#{name}\" -> \"#{cname}\";"
     end
   end
-    
+
   puts "overlap=false"
   puts "label=\"Build dependencies of #{obs.project} #{obs.repo} #{obs.arch}\""
   puts "fontsize=12;"
