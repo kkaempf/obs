cbadce9e239e1b96b1900047f666397573f7ec9e
Klaus Kämpf <kkaempf@suse.de>
2015-09-21 15:44:58 +0200

    Make Product.all a class-function

--- bin/productlist.rb
+++ bin/productlist.rb
@@ -5,14 +5,37 @@
 #
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 
+def usage msg=nil
+  STDERR.puts "Err: #{msg}" if msg
+  STDERR.puts "productlist <project> [-v] [-a <api>]"
+
+  exit (msg)?1:0
+end
+
 require 'obs'
 
 project = ARGV.shift
-raise "No project given" unless project
+usage "No project given" unless project
 
-api = ARGV.shift || "https://api.suse.de"
+api = "https://api.suse.de"
+verbose = nil
+name = nil
 
-product = Obs::Product.new project, :api => api
-all = product.all
+loop do
+  break if ARGV.empty?
+  arg = ARGV.shift
+  case arg
+  when "-v"
+    verbose = true
+  when "-n"
+    name = true
+  when "-a"
+    api = ARGV.shift
+  else
+    usage "Unknown option '#{arg}'"
+  end
+end
+
+all = Obs::Product.all project, :api => api, :verbose => verbose
 
 puts all
--- lib/obs/product.rb
+++ lib/obs/product.rb
@@ -6,117 +6,50 @@
 # See http://api.opensuse.org
 #
 
-require 'getoptlong'
-
 module Obs
 
 class Product
-  require 'net/https'
-  require 'nokogiri'
   
   attr_reader :uri, :user, :password, :name
   
   #
-  # Obs::Project.new name (String), options (Hash) 
+  # Obs::Project.new project (String), options (Hash) 
+  #
+  # Work with products defined in project (typically 'SUSE')
   #
   # option keys:
   #  - :api
   #  - :user
   #  - :password
   #
-  def initialize(product = nil, options = nil)
-    raise ArgumentError.new("Product missing") unless product
+  def initialize(project = nil, options = nil)
+    raise ArgumentError.new("Project missing") unless project
 
-    user = options[:user]
-    password = options[:password]
-    # if user/password not given, try to get them from ~/.oscrc or ~/.netrc
-    if user.nil? || password.nil?
-      user, password = Obs.extract_oscrc @uri
-      if user.nil? || password.nil?
-        user, password = Obs.extract_netrc @uri
-	if user.nil? || password.nil?
-	  raise SecurityError
-	end
-      end
-    end
-    
-    @user = user
-    @password = password
-    @name = product
+    @api = Obs::Api.new(options)
+    @name = project
 
-    @http = Net::HTTP.new @uri.host, @uri.port
-    @http.use_ssl = (@uri.scheme == "https")
-    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   end
   
   def to_s
     @name
   end
-  
-  def api action, url, limit=10
-    raise "http redirection too deep" if limit.zero?
-    c = nil
-    case action.to_sym
-    when :get then     c = Net::HTTP::Get
-    when :put then     c = Net::HTTP::Put
-    when :post then    c = Net::HTTP::Post
-    when :delete then  c = Net::HTTP::Delete
-    when :head then    c = Net::HTTP::Head
-    when :options then c = Net::HTTP::Options
-    else
-      raise "Unknown action #{action}"
-    end
-    req = c.new url
-    req.initialize_http_header({"Accept" => "*/*"})
-    req.basic_auth(@user, @password)
-    @http.start unless @http.started?
-    resp = @http.request( req )
-    
-    raise "Cannot connect to #{url}" unless resp
-#    $stderr.puts "Connected to #{url}"
-    xml = nil
-    if resp.body
-      ct = resp['content-type'].split(';').first # split off ';charset ...'
-#      $stderr.puts "Received #{ct}"
-      case ct
-      when "text/xml", "application/xml"
-        result = Nokogiri::XML(resp.body)
-      when "text/html"
-        result = Nokogiri::HTML(resp.body)
-      when "text/plain"
-        #puts "BODY: '#{resp.body}'"
-      when "application/octet-stream"
-        result = resp.body
-      else
-	$stderr.puts "Unknown content '#{ct}'"
-      end
-    end
-#    $stderr.puts "Parsed #{xml.class}"
-    case resp
-    when Net::HTTPSuccess then      return result
-    when Net::HTTPRedirection then  return api(action, resp['location'], limit-1)
-    when Net::HTTPUnauthorized then raise "Wrong authorization"
-    when Net::HTTPForbidden then    raise "Not allowed"
-    else
-      # FIXME: this must be easier with Nokogiri.
-      if result
-	status = result.xpath("/status/@code").first.text.to_i rescue nil
-	summary = result.xpath("/status/summary").text rescue nil
-#	$stderr.puts "Status '#{status}', Summary '#{summary}'"
-	raise summary if summary
-      else
-	raise "Unknown response #{resp}"
-      end
-      raise resp.to_s
-    end
-    nil
+
+  #
+  # get list of all products
+  #
+  # set options[:verbose] for more details
+  #
+  def self.all project, options = {}
+    api = Obs::Api.new(options)
+    view = options[:verbose] ? "verboseproductlist" : "productlist"
+    api.get "/source/#{project}?view=#{view}&expand=1"
   end
-  
+
   def exists?
     # check access to OBS
 
     begin
-      resp = api :get, "/"
+      resp = @api.get "/"
     rescue Exception => e
       $stderr.puts "Could not access obs server at #{@uri}: #{e}"
       return false
